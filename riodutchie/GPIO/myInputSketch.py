e936aadd7ba144e85c128b9da5a8a42eb3f23ac2
John Wheeler <riodutchie@gmail.com>
2016-09-08 05:05:52 +0000

    added myInputSketch.py

---
+++ riodutchie/GPIO/myInputSketch.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+while True:
+    GPIO.wait_for_edge(23, GPIO.RISING)
+    print ("Button 1 pressed")
+    GPIO.wait_for_edge(23, GPIO.FALLING)
+    print ("Button 1 released")
+    GPIO.wait_for_edge(24, GPIO.FALLING)
+    print ("Button 2 pressed")
+    GPIO.wait_for_edge(24, GPIO.RISING)
+    print ("Button 2 released")
+    exit()
+GPIO.cleanup()
